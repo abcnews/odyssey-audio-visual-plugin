1159a9c835f8d1a875fac520359a52edcf0af93e
Joshua Byrd <phocks@gmail.com>
2020-01-22 15:36:33 +1000

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Odyssey Audio Visual Plugin
+
+A project generated from [aunty](https://github.com/abcnews/aunty)'s `preact` project template.
+
+## Authors
+
+- Joshua Byrd ([phocks@gmail.com](mailto:phocks@gmail.com))
---
+++ src/components/App/index.js
@@ -0,0 +1,44 @@
+import { h, Component } from "preact";
+import { useState, useEffect } from "preact/hooks";
+import styles from "./styles.scss";
+import airpods from "./airpods.svg";
+
+export default props => {
+  const [isMuted, setIsMuted] = useState(false);
+
+  useEffect(() => {
+    console.log("mounted!!!");
+  }, []);
+
+  return (
+    <div className={styles.root}>
+      <div className={`${styles.icon}  ${isMuted && styles.hidden}`}>
+        <img src={airpods} />
+      </div>
+
+      <div className={`${styles.text} ${!isMuted && styles.hidden}`}>
+        KEEP SCROLLING TO READ THE STORY
+      </div>
+
+      <div className={`${styles.text} ${isMuted && styles.hidden}`}>
+        THIS STORY IS BEST EXPERIENCED WITH SOUND ON
+      </div>
+
+      {isMuted ? (
+        <button
+          id="toggle-global-audio-button"
+          onClick={() => setIsMuted(false)}
+        >
+          ENABLE AUDIO
+        </button>
+      ) : (
+        <button
+          id="toggle-global-audio-button"
+          onClick={() => setIsMuted(true)}
+        >
+          MUTE AUDIO
+        </button>
+      )}
+    </div>
+  );
+};
